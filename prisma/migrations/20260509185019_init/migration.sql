9d395fd977c6af554526895576e803337ceb5fee
sawankshrma <thesawan47@gmail.com>
2026-05-10 00:56:05 +0530

    database migrated from sqlite to postgre

---
+++ prisma/migrations/20260509185019_init/migration.sql
@@ -0,0 +1,77 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `authorId` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `content` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `published` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `title` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[username]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `body` to the `Post` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `ownerId` to the `Post` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `username` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- DropIndex
+DROP INDEX "User_email_key";
+
+-- AlterTable
+ALTER TABLE "Post" DROP COLUMN "authorId",
+DROP COLUMN "content",
+DROP COLUMN "published",
+DROP COLUMN "title",
+ADD COLUMN     "body" TEXT NOT NULL,
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "imageUrl" TEXT,
+ADD COLUMN     "ownerId" INTEGER NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "name",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "password" TEXT NOT NULL,
+ADD COLUMN     "profilePicUrl" TEXT,
+ADD COLUMN     "username" TEXT NOT NULL;
+
+-- CreateTable
+CREATE TABLE "_UserFollows" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+
+    CONSTRAINT "_UserFollows_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateTable
+CREATE TABLE "_LikedPosts" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+
+    CONSTRAINT "_LikedPosts_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE INDEX "_UserFollows_B_index" ON "_UserFollows"("B");
+
+-- CreateIndex
+CREATE INDEX "_LikedPosts_B_index" ON "_LikedPosts"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_UserFollows" ADD CONSTRAINT "_UserFollows_A_fkey" FOREIGN KEY ("A") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_UserFollows" ADD CONSTRAINT "_UserFollows_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_LikedPosts" ADD CONSTRAINT "_LikedPosts_A_fkey" FOREIGN KEY ("A") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_LikedPosts" ADD CONSTRAINT "_LikedPosts_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,19 @@
+import { Post } from './generated/prisma/browser';
+import express from "express";
 import { prisma } from "./lib/prisma";
 
-async function main() {
-  // Create a new user with a post
-  const user = await prisma.user.create({
-    data: {
-      name: "Alice",
-      email: "alice@prisma.io",
-      posts: {
-        create: {
-          title: "Hello World",
-          content: "This is my first post!",
-          published: true,
-        },
-      },
-    },
-    include: {
-      posts: true,
-    },
-  });
-  console.log("Created user:", user);
+const app = express();
+app.use(express.json());
 
-  // Fetch all users with their posts
-  const allUsers = await prisma.user.findMany({
-    include: {
-      posts: true,
-    },
-  });
-  console.log("All users:", JSON.stringify(allUsers, null, 2));
-}
+app.get("/api/v1/posts", async (req, res) => {
+  const posts = await prisma.post.findMany();
+  res.json(posts);
+});
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+app.get("/api/v1/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+})
+
+
+app.listen(3001);
